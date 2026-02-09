0b4dd3ab1d7397309c4b3d6ffed15f07cbdd7cbe
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-02-09 09:54:06 -0300

    New Tarefa

---
+++ exrc9.py
@@ -0,0 +1,18 @@
+''''
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+
+''''
+seu_nome = str(input("Digite seu primeiro nome: "))
+
+letras = len(seu_nome) = resultado
+
+if resultado <= 4:
+     print(f'Seu nome {seu_nome}, ele tem {resultado} letras ')
+     print("Ele é curto")
+
+if resultado  = 5 or 6:
+    prints(f"Seu nome {seu_nome}, contem {resultado} letras")
+    print("Seu nome é normal")
+
+else:
+     print("Seu nome é grande")
