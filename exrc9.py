9eb5028d5a200bc9042d283820d3a96376b3d030
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-02-09 10:22:17 -0300

    arrumei o = para ==

--- exrc9.py
+++ exrc9.py
@@ -6,7 +6,7 @@ if resultado <= 4:
      print(f'Seu nome {seu_nome}, ele tem {resultado} letras ')
      print("Ele é curto")
 
-if resultado  = 5 or 6:
+if resultado  == 5 or 6:
     prints(f"Seu nome {seu_nome}, contem {resultado} letras")
     print("Seu nome é normal")
 
