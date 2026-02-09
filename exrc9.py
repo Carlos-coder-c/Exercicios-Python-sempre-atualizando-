3635b7e60ff71c536989a8e1274e1202d72c347d
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-02-09 10:04:18 -0300

    tirando o ''''

--- exrc9.py
+++ exrc9.py
@@ -1,7 +1,3 @@
-''''
-Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
-
-''''
 seu_nome = str(input("Digite seu primeiro nome: "))
 
 letras = len(seu_nome) = resultado
@@ -12,7 +8,7 @@ if resultado <= 4:
 
 if resultado  = 5 or 6:
     prints(f"Seu nome {seu_nome}, contem {resultado} letras")
-    print("Seu nome é normal")
+    print("Seu nome é normal")Tirando ''''
 
 else:
      print("Seu nome é grande")
