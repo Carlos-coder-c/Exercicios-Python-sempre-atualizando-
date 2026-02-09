bb8ab31df179def045bbf3b3ee1a6a3877c4c76b
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-02-09 20:44:38 -0300

    New Past

---
+++ exrc10.py
@@ -0,0 +1,18 @@
+# Declarando Variáveis 
+
+numero = int(input('Digite um número inteiro: '))
+
+num_par = numero / 2
+num_impar = numero not / 2
+
+try:
+if numero == num_par:
+     print(f'O número {numero}, ele é par!')
+ 
+ 
+if numero == num_impar:
+     print(f"O numero {numero}, é impar")
+
+except:
+     print("Você nao digitou número inteiros ")
+  
