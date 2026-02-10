abea7bb9827cf4aa50ee246c7543af704261f986
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-02-09 21:00:03 -0300

    Adicionando paragrafo aos ifs

--- exrc10.py
+++ exrc10.py
@@ -6,11 +6,11 @@ num_par = numero / 2
 num_impar = numero  % 2 != 0
 
 try:
-if numero == num_par:
+ if numero == num_par:
      print(f'O número {numero}, ele é par!')
  
  
-if numero == num_impar:
+ if numero == num_impar:
      print(f"O numero {numero}, é impar")
 
 except:
