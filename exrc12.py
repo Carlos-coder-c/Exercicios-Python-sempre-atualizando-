92a64d636438a34aaeb760f555b66607154ae121
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-10 08:31:16 -0300

    Versão 1 do excr12

---
+++ exrc12.py
@@ -0,0 +1,7 @@
+print('====INICIO FUNÇÕES====')
+
+def apresentar_user(nome, idade, cidade):
+    print(f'Seu nome é {nome}, idade: {idade}, e nasceu na cidade {cidade}')
+
+apresentar_user("Carlos", 13, "Rio Preto")
+apresentar_user("Anna", idade=12, cidade="Rio Preto")
