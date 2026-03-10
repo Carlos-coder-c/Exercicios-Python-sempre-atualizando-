7f7fb0063976a8dee333f354b2b9fff1e0e1a95b
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-10 09:34:49 -0300

    Versão 3: EXRC13: Ajustando SintaxError

--- exrc13.py
+++ exrc13.py
@@ -2,13 +2,13 @@ def gerar_resumo_do_pedido(cliente,valor_produto,produto,frete=5, desconto=10):
     subtotal = valor_produto
     total = subtotal + frete - desconto
 
-    print(f"RESUMO: \
+    print(f"""RESUMO: 
     CLIENTE: {cliente},
     PRODUTO: {produto}, {valor_produto},
     SUBTOTAL: {subtotal}, 
     FRETE: {frete},
     DESCONTO: {desconto},
     TOTAL: {total},
-       ")
+       """)
 
 gerar_resumo_do_pedido(cliente="Carlos", valor_produto=20, produto="Teclado USB")
