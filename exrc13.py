ce60d06f1be110eebf3e88a461826eecd98e26f2
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-10 09:30:11 -0300

    Ajustando os parâmetros e argumentos

--- exrc13.py
+++ exrc13.py
@@ -1,4 +1,4 @@
-def gerar_resumo_do_pedido(cliente,valor_produto,produto, subtotal, frete=5, desconto=10, total):
+def gerar_resumo_do_pedido(cliente,valor_produto,produto,frete=5, desconto=10):
     subtotal = valor_produto
     total = subtotal + frete - desconto
 
@@ -6,9 +6,9 @@ def gerar_resumo_do_pedido(cliente,valor_produto,produto, subtotal, frete=5, des
     CLIENTE: {cliente},
     PRODUTO: {produto}, {valor_produto},
     SUBTOTAL: {subtotal}, 
-    FRETE: {frete=},
-    DESCONTO: {desconto=},
+    FRETE: {frete},
+    DESCONTO: {desconto},
     TOTAL: {total},
        ")
 
-gerar_resumo_do_pedido(cliente="Carlos", valor_produto=20, produto="Teclado USB", subtotal, frete, desconto, total)
+gerar_resumo_do_pedido(cliente="Carlos", valor_produto=20, produto="Teclado USB")
