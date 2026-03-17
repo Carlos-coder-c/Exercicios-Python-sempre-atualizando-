3b952e9ac384e48eeb4881613013d5e57d9babee
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-17 08:40:48 -0300

    Versão 2: Debbug simples no nameError e testando nova possibilidade

--- exrc15.py
+++ exrc15.py
@@ -1,11 +1,11 @@
 # Criar uma função que terone se o número é Par ou Ímpar 
 
 def par_impar(numero):
-    numero_par = numero % 2
+    numero_par = numero % 2 == 0
 
     if numero_par == 0:
-        return f'O Numero é par'
-    return 'Número é impar'
+        return f'O Numero {numero} é par'
+    return f'Número {numero} é impar'
 
-resultado = par_impar(10)
-print(resultado, numero)
+resultado = par_impar
+print(resultado(3))
