cb2b34d778b55a2d71cf7429abec5accccd731c0
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-17 08:25:43 -0300

    Versão 1: Exrc14

---
+++ exrc14.py
@@ -0,0 +1,11 @@
+# Objetivo é criar uma função que multiplique todos os argumentos nao nomeados e retorne o valor em uma variável 
+
+def multiplicar(*args):
+    total = 0
+    for numero in args:
+        total *= numero
+        return total
+
+resultado = multiplicar(2, 3, 7, 8, 34)
+print(resultado)
+
