536794b761cf6904235332fa05f371e8edeca2f4
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-17 08:28:35 -0300

    Versao 1: Exrc14 - Alterando valor de total

--- exrc14.py
+++ exrc14.py
@@ -1,10 +1,10 @@
 # Objetivo é criar uma função que multiplique todos os argumentos nao nomeados e retorne o valor em uma variável 
 
 def multiplicar(*args):
-    total = 0
+    total = 1
     for numero in args:
         total *= numero
-        return total
+    return total
 
 resultado = multiplicar(2, 3, 7, 8, 34)
 print(resultado)
