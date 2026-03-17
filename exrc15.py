c4a937b653cf3b0c27caa020577a3a5cfe8eab30
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-17 08:35:09 -0300

    Versao 1: Exrc 15

---
+++ exrc15.py
@@ -0,0 +1,11 @@
+# Criar uma função que terone se o número é Par ou Ímpar 
+
+def par_impar(numero):
+    numero_par = numero % 2
+
+    if numero_par == 0:
+        return f'O Numero é par'
+    return 'Número é impar'
+
+resultado = par_impar(10)
+print(resultado)
